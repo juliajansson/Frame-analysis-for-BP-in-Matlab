e64eadfcc2acd05be8d0bad84e986e6ef7f3ca35
Julia Jansson <julia.jansson@gmail.com>
2016-07-11 19:08:04 +0200

    Update Contrast.m

--- Contrast.m
+++ Contrast.m
@@ -5,8 +5,6 @@ filename=['/Users/samueltober/Desktop/Referens_filmer/Bea/121_75/' t(i,:)]
 
 y=imread(filename);
 
-c = 0;
-
 y=double(y);
 
 b=y(:,:,3);
@@ -16,10 +14,6 @@ r=y(:,:,1);
 r=r/max(max(r));
 g=g/max(max(g));
 b=b/max(max(b));
-
-bb=adapthisteq(b, 'clipLimit',c);
-gg=adapthisteq(g, 'clipLimit',c);
-rr=adapthisteq(r, 'clipLimit',c);
 
 rgb(:,:,1)=rr;
 rgb(:,:,2)=gg;
