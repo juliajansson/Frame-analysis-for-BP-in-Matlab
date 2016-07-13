483446c7a9385a9121229755ec9d496f0e4e23fe
Julia Jansson <julia.jansson@gmail.com>
2016-07-13 14:31:05 +0200

    Update RGB.m

--- RGB.m
+++ RGB.m
@@ -1,20 +1,19 @@
 function [rgb] = RGB(bild,x_1,x_2)
-%Kom ihåg att ändra koordinatsiffrorna här!
 
-im=bild; %L�ser in bilden %im=imread(namn); innan
-y_1 = 250; %Mellan vilka v�rden som bilden ska zoomas
+im=bild; %Reads in the image
+y_1 = 250; %The y values that the image should be zoomed within
 y_2 = 350;
 
-bildzoom=im(y_1:y_2,x_1:x_2,:); %skapar en ny, zoomad bild
+bildzoom=im(y_1:y_2,x_1:x_2,:); %Creates a new, cropped pictur
 
-r=bildzoom(:,:,1); %tar ut r,g och b matriser ur zoom bilden
+r=bildzoom(:,:,1); %Takes out r,g och b matrices from the cropped picture
 g=bildzoom(:,:,2);
 b=bildzoom(:,:,3);
 
-r_mean = mean(mean(r));
+r_mean = mean(mean(r)); %Calculates the means of the layers
 g_mean = mean(mean(g));
 b_mean = mean(mean(b));
 
-rgb=[r_mean;g_mean;b_mean];
+rgb=[r_mean;g_mean;b_mean]; %Creates a new matrix with the means of the layers
 
 end
